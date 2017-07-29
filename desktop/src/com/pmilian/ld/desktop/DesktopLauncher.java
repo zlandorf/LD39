8be949c432a47bf343b6a8c63246868b85badd3c
zlandorf <paul.milian@hotmail.fr>
2017-07-29 11:15:04 +0200

    initial commit

---
+++ desktop/src/com/pmilian/ld/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pmilian.ld.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pmilian.ld.LD39;
+
+public class DesktopLauncher {
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new LD39(), config);
+    }
+}
---
+++ html/src/com/pmilian/ld/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.pmilian.ld.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.pmilian.ld.LD39;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new LD39();
+        }
+}
