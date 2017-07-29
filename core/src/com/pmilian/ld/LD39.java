acbe754408c6cf9fc1937bb360f87a1005a26696
zlandorf <paul.milian@hotmail.fr>
2017-07-29 11:28:37 +0200

    add initial sprites

--- core/src/com/pmilian/ld/LD39.java
+++ core/src/com/pmilian/ld/LD39.java
@@ -4,16 +4,22 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class LD39 extends ApplicationAdapter {
     SpriteBatch batch;
     Texture img;
+    TextureAtlas atlas;
+    Sprite player;
     
     @Override
     public void create () {
         batch = new SpriteBatch();
         img = new Texture("badlogic.jpg");
+        atlas = new TextureAtlas("sprites.txt");
+        player = atlas.createSprite("player");
     }
 
     @Override
@@ -22,6 +28,7 @@ public class LD39 extends ApplicationAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(img, 0, 0);
+        player.draw(batch);
         batch.end();
     }
     
@@ -29,5 +36,6 @@ public class LD39 extends ApplicationAdapter {
     public void dispose () {
         batch.dispose();
         img.dispose();
+        atlas.dispose();
     }
 }
